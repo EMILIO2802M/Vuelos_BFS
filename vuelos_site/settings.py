d515e08b27a59de9bfcd99403ad58592f2a9fb51
EMILIO2802M <emilio2802m@gmail.com>
2026-03-19 08:23:09 -0600

    Actualización del proyecto

--- vuelos_site/settings.py
+++ vuelos_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ey3r682b!xdk5l))igk$13$%c@x!9i8jxksdvqz7z37+20_t=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -116,3 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
